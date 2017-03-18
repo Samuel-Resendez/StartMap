d2038cff3a7bc6b935838d34e2014d9f8cc8d6bf
Samuel Resendez <saresend@usc.edu>
2017-03-18 16:36:42 +0100

    found offset

--- assets/scripts.js
+++ assets/scripts.js
@@ -3,7 +3,7 @@
 
 var login = function() {
   var db_url = "https://simulator-api.db.com/gw/oidc/authorize?response_type=code&redirect_uri=/authorized.html&client_id=435eb207-29a0-4b46-9083-8309cd28f457&state=/authorized.html";
-  fetch(db_url)  
+  fetch(db_url)
   .then(
     function(response) {
       if (response.status !== 200) {
